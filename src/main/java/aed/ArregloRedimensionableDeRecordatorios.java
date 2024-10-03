dc2d7e8e5117a196946fd112f8e42bcd7e1001d6
Rodrycue18 <rodrigosaucedo2002@gmail.com>
2024-10-03 00:54:16 -0300

    Codigo de Arreglo que pasa los tests

--- src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
+++ src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
@@ -1,39 +1,65 @@
 package aed;
 
 class ArregloRedimensionableDeRecordatorios {
-
+    private Recordatorio[] seq;
     public ArregloRedimensionableDeRecordatorios() {
         // Implementar
+        this.seq = new Recordatorio[0]; //Cosntructor de lista de Recordatorios vacia
+        // Para evitar aliasing usamos new 
+
     }
 
     public int longitud() {
         // Implementar
-        return -1;
+        return this.seq.length;
     }
 
     public void agregarAtras(Recordatorio i) {
         // Implementar
+        Recordatorio[] nuevaLista = new Recordatorio[this.seq.length+1]; //La nueva lista tiene +1 tamanio
+        for (int j=0;j<this.seq.length;j++){
+            nuevaLista[j] = this.seq[j]; 
+        }
+        
+        nuevaLista[nuevaLista.length-1] = i;
+
+        this.seq = nuevaLista.clone();
+        // Actualizo atrributo
     }
 
     public Recordatorio obtener(int i) {
         // Implementar
-        return null;
+        return this.seq[i];
     }
 
     public void quitarAtras() {
         // Implementar
+        Recordatorio[] nuevaListaConUnoMenos = new Recordatorio[this.seq.length-1];
+        for (int i=0; i<this.seq.length-1;i++){
+            nuevaListaConUnoMenos[i]=this.seq[i];
+        }
+        this.seq = nuevaListaConUnoMenos.clone();
+
     }
 
     public void modificarPosicion(int indice, Recordatorio valor) {
         // Implementar
+        this.seq[indice] = valor;
     }
 
     public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
-        // Implementar
+        // Osea esto es un constructor , que de parametro acepta la misma clase, le paso la lista llena , basicamente
+        this.seq = new Recordatorio[vector.longitud()];
+        for (int i=0;i<vector.longitud();i++){
+            this.seq[i] = vector.obtener(i);
+        }
+        //Osea este constructor evita que haya un aliasing entre 2 objetos??
     }
 
     public ArregloRedimensionableDeRecordatorios copiar() {
         // Implementar
-        return null;
+        return new ArregloRedimensionableDeRecordatorios(this); //LLegue bien hasta retornar una instancia de 
+                                                                //mi constructor que recibe de parametro un vector, pero el this??????
+                                                                //se lo pregunte a chatgpt, como funciona esta wea de this ??
     }
 }
