5eabdc4cd95c18c821a73d748ab86dfb4a9d0f80
Rodrycue18 <rodrigosaucedo2002@gmail.com>
2024-10-04 00:09:12 -0300

    Finalmente acabamos esta maldito taller la ptm , como costo

--- src/main/java/aed/Agenda.java
+++ src/main/java/aed/Agenda.java
@@ -3,29 +3,41 @@ package aed;
 public class Agenda {
     private Fecha fecha;
     private Recordatorio rec;
-    //private ArregloRedimensionableDeRecordatorios seq;
+    private ArregloRedimensionableDeRecordatorios seq = new ArregloRedimensionableDeRecordatorios();
     public Agenda(Fecha fechaActual) {
         // Implementar
-        this.fecha = new Fecha(fechaActual);// Instacio la clase Fecha donde le paso algo de tipo Fecha
+        this.fecha = new Fecha(fechaActual);// Instancio la clase Fecha donde le paso algo de tipo Fecha
     }
 
     public void agregarRecordatorio(Recordatorio recordatorio) {
         // Implementar //"Parece constructor de Recordatorio"
         // AGREGAMOS RECORDATORIO A LA CLASE ARREGLO
         this.rec = recordatorio;
-      //  this.seq.agregarAtras(this.rec);
+        this.seq.agregarAtras(this.rec);
     }
 
     @Override
     public String toString() {
         // Implementar
         //DEBEMOS HACER QUE TODOS LOS ELEMENTOS DEL ARREGLO SE PRINTEEN DE UNA
-        //String res = this.fecha + "\n" + "=====" +"\n" + this.seq.obtener(0)
-        return this.fecha + "\n" + "=====" +"\n" ;//+ this.seq.obtener(0) +"\n"+ this.seq.obtener(1);
+        // Creamos un objeto string para agregar a gusto los recordatorios
+
+        String resultado = new String();
+        resultado = this.fecha + "\n" + "=====" +"\n";
+        // Reviso con fechaIndividual si cada uno de los recordatorios de la lista son iguales , si son iguales entonces solo los elementos con fecha igual se van a Printear ,e ignorara si alguna tiene fecha diferente a la actual
+        for (int i = 0; i<this.seq.longitud();i++){
+            Recordatorio fechaIndividual = this.seq.obtener(i);
+            if (fechaIndividual.fecha().equals(this.fecha)){
+                resultado = resultado + this.seq.obtener(i) + "\n";
+                }
+        }
+        
+        return resultado;
     }
 
     public void incrementarDia() {
         // Implementar
+        this.fecha.incrementarDia();
     }
 
     public Fecha fechaActual() {
